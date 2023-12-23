c62f670fc86e4d170191fb1c4d4712a6c6fe6085
Rida Khan <122581057+katastrophe-codes@users.noreply.github.com>
2023-12-22 18:40:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import sklearn
-import scipy
+!pip install scipy
+!pip install sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.sparse import coo_matrix
